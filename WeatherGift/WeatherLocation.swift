c7e3631c9a3067d1807f0b20720f87a7de87a955
Brenden Picioane <brenden.picioane@gmail.com>
2020-10-07 23:28:34 -0400

    finished 6.2

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Brenden Picioane on 10/7/20.
+//  Copyright © 2020 Brenden Picioane. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}

22fd2cdb5b62e8f17645c3de0a1d949f322745e5
Brenden Picioane <brenden.picioane@gmail.com>
2020-10-08 00:07:37 -0400

    with .gitignore

---
+++ WeatherGift/APIkeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIkeys.swift
+//  WeatherGift
+//
+//  Created by Brenden Picioane on 10/8/20.
+//  Copyright © 2020 Brenden Picioane. All rights reserved.
+//
+
+import Foundation
+
+struct APIkeys {
+    static let googlePlacesKey = "AIzaSyBBeJ7XqDBMi6wV2AJJyY_odN-06pd1wk8"
+}

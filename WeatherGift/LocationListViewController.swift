395d95c6e6f7038fcc89b0e0598dab1be1605519
Brenden Picioane <brenden.picioane@gmail.com>
2020-10-09 10:50:17 -0400

    finished 6.7

--- WeatherGift/LocationListViewController.swift
+++ WeatherGift/LocationListViewController.swift
@@ -21,19 +21,23 @@ class LocationListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-//        var weatherLocation = WeatherLocation(name: "Chestnut Hill, MA", latitude: 0, longitude: 0)
-//        weatherLocations.append(weatherLocation)
-//        weatherLocation = WeatherLocation(name: "Middle Village, NY", latitude: 0, longitude: 0)
-//        weatherLocations.append(weatherLocation)
-//        weatherLocation = WeatherLocation(name: "Mountain View, CA", latitude: 0, longitude: 0)
-//        weatherLocations.append(weatherLocation)
         
         tableView.dataSource = self
         tableView.delegate = self
     }
     
+    func saveLocations() {
+        let encoder = JSONEncoder()
+        if let encoded = try? encoder.encode(weatherLocations) {
+            UserDefaults.standard.set(encoded, forKey: "weatherLocations")
+        } else {
+            print("L. saving didn't work.")
+        }
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         selectedLocationIndex = tableView.indexPathForSelectedRow!.row
+        saveLocations()
     }
     
     @IBAction func editButtonPressed(_ sender: UIBarButtonItem) {

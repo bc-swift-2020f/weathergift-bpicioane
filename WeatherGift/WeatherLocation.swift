d6f9bea72567ce56583174b7e7e1d23d6ad6c0ad
Brenden Picioane <brenden.picioane@gmail.com>
2020-10-09 16:49:44 -0400

    finished 6.10

--- WeatherGift/WeatherLocation.swift
+++ WeatherGift/WeatherLocation.swift
@@ -18,4 +18,26 @@ class WeatherLocation: Codable {
         self.latitude = latitude
         self.longitude = longitude
     }
+    
+    func getData() {
+        let urlString = "https://api.openweathermap.org/data/2.5/onecall?lat=\(latitude)&lon=\(longitude)&exclude=minutely&units=imperial&appid=\(APIkeys.openWeatherKey)"
+        guard let url = URL(string: urlString) else {
+            print("L. Couldn't create URL.")
+            return
+        }
+        print(url)
+        let session = URLSession.shared
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print("L. \(error.localizedDescription)")
+            }
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!, options: [])
+                print("W. \(json)")
+            } catch {
+                print("JSON L. \(error.localizedDescription)")
+            }
+        }
+        task.resume()
+    }
 }
